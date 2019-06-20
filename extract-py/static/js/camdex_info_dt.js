be16a279313b89d40ee37a70c7c09b1d094491ac
Omar Mendo <omen.mesa20@gmail.com>
2019-06-20 22:40:38 +0100

    Creando tablas de ejemplo

---
+++ extract-py/static/js/camdex_info_dt.js
@@ -0,0 +1,13 @@
+// Patients information datatable
+
+$(document).ready( function () {
+    $('#camdex_patient').DataTable({
+        language: {url: "//cdn.datatables.net/plug-ins/a5734b29083/i18n/Spanish.json"},
+        pageLength: 10,
+        scrollY: '45vh',
+        scrollCollapse: true,
+        autoWidth: true,
+        paging: false,
+        searching: false,
+    } );
+} );
