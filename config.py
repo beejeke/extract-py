798e77096cee1fe83e08f73268585ae61895c6ae
Omar Mendo <omen.mesa20@gmail.com>
2019-06-21 20:44:43 +0100

    Modificando configuración

--- config.py
+++ config.py
@@ -1,16 +1,23 @@
 # Configuration
 
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "185402ba04e44f53a0845a67c014b411"
 
 
 class DevelopmentConfig(BaseConfig):
     ENV = "development"
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///extract-py/develop.db"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{ROOT_DIR}/develop.db"
     SECRET_KEY = "08390129adaea24e1c0a3b5b"
 
 
