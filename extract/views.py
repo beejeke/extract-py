a711402a077da016985284a07c9c8d85b48956f6
Omar Mendo <omen.mesa20@gmail.com>
2019-07-25 14:07:30 +0100

    Añadiendo funcionalidad de login y registro - ✅

--- extract/views.py
+++ extract/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, render_template
+from flask import (Blueprint, jsonify, render_template, redirect, url_for, logging, request)
 
 from extract.model import CamdexData, Patient, User, Variable, db
 from extract.schema import PatientInfo
@@ -11,7 +11,33 @@ def index():
     return render_template('index.html')
 
 
-@frontend.route('/patients/')
+@frontend.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        userlog = request.form["userlog"]
+        pwdlog = request.form["pwdlog"]
+
+        login = User.query.filter_by(username=userlog, password=pwdlog).first()
+        if login is not None:
+            return redirect(url_for("frontend.patients"))
+    return render_template("login.html")
+
+
+@frontend.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        userreg = request.form['userreg']
+        pwdreg = request.form['pwdreg']
+
+        register = User(username=userreg, password=pwdreg)
+        db.session.add(register)
+        db.session.commit()
+
+        return redirect(url_for("frontend.patients"))
+    return render_template("register.html")
+
+
+@frontend.route('/patients')
 def patients():
     patient = Patient.query.order_by(Patient.name).all()
     return render_template('patients.html', patient=patient)
